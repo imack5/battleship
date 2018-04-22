05da05a22519be7a6e1bbfd817db8d639a54e076
imack5 <iainsmackenzie@gmail.com>
2018-04-22 18:28:20 +0000

    Added a way to enter start and end locations of pieces

--- battleship.js
+++ battleship.js
@@ -1,14 +1,38 @@
+var prompt = require("prompt-sync")();
+
+
+
+let playingPieces = {
+
+  carrier: {id: 'CA'},
+  //battleship: {id: 'BA'},
+  //cruiser: {id: 'CR'},
+  //submarine: {id: 'SU'},
+  //destroyer: {id: 'DE'}
+
+};
+
+
 let playingField = [
                 [0, 0, 0, 0, 0],
-                [1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0]
                ];
 
+function placePieces(){
+
+  for(let piece in playingPieces){
+    let location = prompt(`Please enter ${piece} start location (ie: A2)`);
+    playingPieces[piece].start = location.split('');
+    location = prompt(`Please enter ${piece} end location (ie: A2)`);
+    playingPieces[piece].end = location.split('');
+  }
+}
 
 
-//console.log(playingField);
+
 
 function printPlayingBoard(field){
   let board = '';
@@ -24,4 +48,6 @@ function printPlayingBoard(field){
   console.log(board);
 }
 
+placePieces();
+console.log(playingPieces);
 printPlayingBoard(playingField);
